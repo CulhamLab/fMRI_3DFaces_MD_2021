987765abb9798d6a72b11d77546a404828a6e2ea
kmstubbs <kmstubbs@gmail.com>
2021-08-24 22:54:43 -0400

    resize images before running experiment

---
+++ Experiment/Images_PILOT/copy_and_resize_images.m
@@ -0,0 +1,17 @@
+RESIZE_FACTOR = 0.25;
+FOLDER_RAW = [pwd filesep 'Raw' filesep];
+
+list = dir(FOLDER_RAW);
+list = list([list.bytes]>0);
+
+number_files = length(list);
+fprintf('Applying resize factor %g to %d images...\n', RESIZE_FACTOR, number_files);
+for fid = 1:number_files
+    fprintf('Processing %d of %d: %s\n', fid, number_files, list(fid).name);
+    
+    img = imread([list(fid).folder filesep list(fid).name]);
+    img = imresize(img, RESIZE_FACTOR);
+    imwrite(img, list(fid).name);
+end
+
+disp Done!
