abf54ebc05ad783eeb89efc6f0e9499d309bde65
CulhamLabGeneral_Tiberius <40447004+CulhamLabGeneral@users.noreply.github.com>
2022-01-18 12:26:24 -0500

    flipped stereo
    flipped stereo to correct

--- Experiment/RunExperiment.m
+++ Experiment/RunExperiment.m
@@ -68,8 +68,8 @@ p.SCREEN.EXPECTED_SIZE = [1080 1920]; %[height width]
 %stereo
 p.SCREEN.STEREO_MODE = 1; %1 or 11 for shutter glasses (1 seems to work better)
 p.SCREEN.PIPELINE_MODE = kPsychNeedFastBackingStore;
-p.SCREEN.BUFFER_ID.LEFT = 1; %flip these if L/R is reversed
-p.SCREEN.BUFFER_ID.RIGHT = 0;
+p.SCREEN.BUFFER_ID.LEFT = 0; %flip these if L/R is reversed
+p.SCREEN.BUFFER_ID.RIGHT = 1;
 
 %image
 p.IMAGES.EXPECTED_WIDTH = 1920;
